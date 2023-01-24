70a312e454989c0d96fa438fbb52fea7177765c9
prashant3jan <prashant3jan@gmail.com>
2023-01-24 14:19:49 +0530

    adding swagger ui config example

--- src/main/java/guru/springfamework/config/SwaggerConfig.java
+++ src/main/java/guru/springfamework/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package guru.springfamework.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -9,7 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig{ //extends WebMvcConfigurationSupport {
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
@@ -18,4 +20,12 @@ public class SwaggerConfig {
                 .build()
                 .pathMapping("/");
     }
+
+//    @Override
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars");
+//    }
 }
