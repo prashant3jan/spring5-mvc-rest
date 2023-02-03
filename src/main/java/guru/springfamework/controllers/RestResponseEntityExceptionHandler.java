2fedf147910db9bf3778f8d5cf80b6c443aa3604
prashant3jan <prashant3jan@gmail.com>
2023-02-03 14:13:24 +0530

    adding controller advice annotation

--- src/main/java/guru/springfamework/controllers/RestResponseEntityExceptionHandler.java
+++ src/main/java/guru/springfamework/controllers/RestResponseEntityExceptionHandler.java
@@ -5,9 +5,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
+@RestControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler({ResourceNotFoundException.class})
     public ResponseEntity<Object> handleNotFoundException(Exception exception, WebRequest request){
